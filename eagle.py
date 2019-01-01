61b6b9bb4895a5477b67333faffdee72b87327b7
mohtork <muhtork@gmail.com>
2019-01-01 19:13:42 +0300

    initial commit

---
+++ eagle.py
@@ -0,0 +1,68 @@
+#!/usr/bin/python
+import socket
+import time
+import os
+
+from tools import resources as res
+import config as cf
+import messages as msg
+import alerts as alert
+
+
+x = res.Resources()
+hostname=(socket.gethostname())
+date=time.time()
+
+def GetDisks():
+	values=[]
+	for k, v in cf.yamls['disks'].items():
+		values.append(v)
+	return values
+
+def GetCpuAvg():
+	interval=cf.yamls['cpu']['cpu_avg_interval']
+	try:
+		if interval==1:
+			cpu_avg=x.LoadAvarage(0)
+		elif interval==5:
+			cpu_avg=x.LoadAvarage(1)
+		elif interval==15:
+			cpu_avg=x.LoadAvarage(2)
+		return cpu_avg
+	except:
+		return 'cpu_avg_interval not configured well'
+
+def CpuMsg():
+	cpu=GetCpuAvg()
+	critical=cf.yamls['cpu']['warning']		
+
+	if cpu>=critical:
+		danger=msg.CpuLoadAvg(str(cpu), hostname)
+		return alert.Slack(danger, 'Alarm', 'danger', 'high', str(date))	
+def MemoryMsg():
+	memory=x.MemoryUsage()
+	if memory['percent']>=90:
+                danger=msg.MemoryPercent(memory['percent'], hostname)
+                return alert.Slack(danger, 'Alarm', 'danger', 'high', str(date))
+
+def SwapMsg():
+	swap=x.Swap()
+	if swap['percent']>=90:
+                danger=msg.SwapPercent(swap['percent'], hostname)
+                return alert.Slack(danger, 'Alarm', 'danger', 'high', str(date))
+def DiskMsg():
+        partitions=GetDisks()
+        for partition in partitions:
+                percent = x.MapDiskUsage()[partition][3]
+                if percent >= 90:
+                        health=partition
+                        warn=msg.DiskPercent(partition, percent, hostname)
+                        return alert.Slack(warn , 'Alarm', 'danger', 'high', str(date))
+
+def Main():
+	CpuMsg()
+	MemoryMsg()
+	SwapMsg()
+	DiskMsg()
+if __name__ == '__main__':
+	Main()
---
+++ messages.py
@@ -0,0 +1,20 @@
+from blessings import Terminal
+
+def DiskPercent(partition, percent, hostname):
+	warn =  partition +' has reached '+ str(percent)+ '%'+' on '+ hostname
+	return warn
+
+def MemoryPercent(percent, hostname):
+	percent=percent
+	hostname=hostname
+	danger="The memory usage has reached "+str(percent)+"% on "+hostname 
+	return danger
+
+def SwapPercent(percent, hostname):
+	danger="The swap usage has reached "+str(percent)+"% on "+hostname
+	return danger
+
+def CpuLoadAvg(avarage, hostname):
+	danger="average cpu load is "+str(avarage)+" on "+hostname
+	return danger
+
